db6fe801298396b889599336c7058c84810de34f
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-27 23:50:54 +0700

    Link/Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,31 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const router = useRouter();
+    const handleClickUrl = (url: string) => {
+        router.push(url);
+    };
     return (
         <html>
             <head />
-
             <body>
-                <div>Header</div>
+                <ul>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/dashboard">Dashboard</Link>
+                    </li>
+                    <li onClick={() => handleClickUrl("/settings")}>
+                        Settings
+                    </li>
+                </ul>
                 {children}
                 <div>Footer</div>
             </body>
